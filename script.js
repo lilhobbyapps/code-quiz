539f6db33555c778bdb55e3da4d8d0ecc58888af
Lil Hobby <gregbedford93@gmail.com>
2023-07-06 20:46:17 -0400

    fixed timer bug

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 var h1 = document.querySelector("h1");
 var mainText = document.querySelector("#mainText");
 var timerText = document.getElementById("timerText");
-var timer = 100;
-
+var counter = 10;
   // Start Quiz
 var startQuizButton = document.querySelector("#buttonStartQuiz");
 startQuizButton.addEventListener("click", startQuiz);
 
+
+
+
 function startQuiz() {
-    timer = 100;
-    timerText.innerText = timer;
-    startTimer;
+    counter = 10;
+    startQuizButton.style.display = "none";
+    var timerCountdown = setInterval(function(){
+      if (counter >= 0) {
+        timerText.innerHTML = counter;
+        counter -= 1;
+      } else {
+        clearInterval(timerCountdown);
+        startQuizButton.style.display = "block";
+      }
+    }, 1000);
 }
 
-var startTimer = setInterval(function(){
-        
-    if(timer > 0) {
-        timer -= 1;
-        timerText.innerText = timer;
-    } else {
-        timer = 0;
-        clearInterval();
-    }
 
-}, 1000);
 
 
 // QUESTIONS/OPTIONS/ANSWER
